349dd664b86b66d1077ee9afef15e79a2ed9c0f5
spaomalley <spaomalley@gmail.com>
2018-12-26 14:28:08 -0500

    Without Node

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+
+function Payment()
+{
+
+}
+
+function Customer()
+{
+
+}
+
+function Transaction()
+{
+
+}
+
+function demoAPI(){
+    
+    var DA = new API_Demo();
+    DA.setTokens();
+    DA.storedCardPayment();
+}
---
+++ scripts/vendorLoad.js
@@ -0,0 +1,9 @@
+
+
+
+function vloader()
+{
+    var merchantAuthenticationType = new ApiContracts.MerchantAuthenticationType();
+    merchantAuthenticationType.setName('YOUR_API_LOGIN_ID');
+    merchantAuthenticationType.setTransactionKey('YOUR_TRANSACTION_KEY');
+}
